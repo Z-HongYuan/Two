f6941d87f0883672e58d0b97ea895ac202ade350
Z-HongYuan <1939218189@qq.com>
2025-06-10 19:48:19 +0800

    添加SDFComponent

---
+++ Source/TwoGame/Component/TwoSDFComponent.cpp
@@ -0,0 +1,77 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+
+#include "TwoSDFComponent.h"
+
+#include "ComponentUtils.h"
+
+
+UTwoSDFComponent::UTwoSDFComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+	SetIsReplicated(false);
+
+	bHiddenInGame = true;
+	FaceForwardValueName = "SDF_F";
+	FaceRightValueName = "SDF_R";
+	FaceForwardName = "SDF_F";
+	FaceRightName = "SDF_R";
+	HeadName = "Head";
+}
+
+
+void UTwoSDFComponent::RefreshFaceMaterialInstance()
+{
+	if (FaceMaterialInstance != nullptr)
+	{
+		//TODO 调研是否需要ConditionalBeginDestroy,重置指针还是删除指针指向的材质
+		FaceMaterialInstance->ConditionalBeginDestroy();
+		//log
+		UE_LOG(LogTemp, Warning, TEXT("UGenshinSDFComponent::RefreshFaceMaterialInstance() FaceMaterialInstance is BeginDestroy"));
+	}
+
+	ParentMeshComponent = Cast<USkeletalMeshComponent>(ComponentUtils::GetAttachedParent(this));
+
+	if (ParentMeshComponent.Get())
+	{
+		//TODO 父类没有材质可供创建动态的时候,会崩溃
+		FaceMaterialInstance = ParentMeshComponent.Get()->CreateDynamicMaterialInstance(
+			FaceID, ParentMeshComponent.Get()->GetMaterial(FaceID), "FaceMI");
+		//log
+		UE_LOG(LogTemp, Log, TEXT("UGenshinSDFComponent::RefreshFaceMaterialInstance() FaceMaterialInstance is Created"));
+	}
+}
+
+void UTwoSDFComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	RefreshFaceMaterialInstance();
+}
+
+void UTwoSDFComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (FaceMaterialInstance != nullptr)
+	{
+		FaceMaterialInstance->ConditionalBeginDestroy();
+		UE_LOG(LogTemp, Log, TEXT("UGenshinSDFComponent::EndPlay() FaceMaterialInstance is BeginDestroy"));
+	}
+
+	ParentMeshComponent = nullptr;
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void UTwoSDFComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	const FVector Forward = ParentMeshComponent->GetSocketLocation(FaceForwardName);
+	const FVector Right = ParentMeshComponent->GetSocketLocation(FaceRightName);
+	const FVector Head = ParentMeshComponent->GetSocketLocation(HeadName);
+
+	if (FaceMaterialInstance and ParentMeshComponent.IsValid())
+	{
+		FaceMaterialInstance->SetVectorParameterValue(FaceForwardValueName, (Forward - Head).GetSafeNormal());
+		FaceMaterialInstance->SetVectorParameterValue(FaceRightValueName, (Right - Head).GetSafeNormal());
+	}
+}
---
+++ Source/TwoGame/Component/TwoSDFComponent.h
@@ -0,0 +1,58 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "TwoSDFComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TWOGAME_API UTwoSDFComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTwoSDFComponent();
+
+	//脸部材质ID
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	int FaceID;
+
+	//需要变化的材质属性名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceForwardValueName;
+
+	//需要变化的材质属性名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceRightValueName;
+
+	//骨骼插槽名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceForwardName;
+
+	//骨骼插槽名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName FaceRightName;
+
+	//骨骼插槽名字
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SDF")
+	FName HeadName;
+
+	//刷新面部材质实例,在更换角色的时候非常有用
+	UFUNCTION(BlueprintCallable, Category = "SDF")
+	void RefreshFaceMaterialInstance();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	UPROPERTY()
+	TWeakObjectPtr<USkeletalMeshComponent> ParentMeshComponent;
+
+	UPROPERTY(Transient)
+	TObjectPtr<UMaterialInstanceDynamic> FaceMaterialInstance;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+};
