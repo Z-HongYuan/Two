f761d9e9a9f0cefcb3822f2eb7763bd2b9c824c0
Z-HongYuan <1939218189@qq.com>
2025-06-10 20:39:47 +0800

    添加Animation的Tag绑定

---
+++ Source/TwoGame/Animation/TwoAnimInstanceBase.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+
+#include "TwoAnimInstanceBase.h"
+#include "AbilitySystemGlobals.h"
+
+void UTwoAnimInstanceBase::SetGameplayTagPropertyMap(AActor* ListenInActor)
+{
+	if (UAbilitySystemComponent* ASC = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(ListenInActor))
+	{
+		check(ASC);
+		GameplayTagPropertyMap.Initialize(this, ASC);
+	}
+}
---
+++ Source/TwoGame/Animation/TwoAnimInstanceBase.h
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffectTypes.h"
+#include "Animation/AnimInstance.h"
+#include "TwoAnimInstanceBase.generated.h"
+
+/**
+ * 带有Tag绑定的AnimInstance
+ */
+UCLASS()
+class TWOGAME_API UTwoAnimInstanceBase : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	//绑定bool和Tag的函数
+	UFUNCTION(BlueprintCallable, Category = "GameplayTags")
+	void SetGameplayTagPropertyMap(AActor* ListenInActor);
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "GameplayTags")
+	FGameplayTagBlueprintPropertyMap GameplayTagPropertyMap;
+};
