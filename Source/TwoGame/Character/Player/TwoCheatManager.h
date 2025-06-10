f1d2e52643354b224300078de2bfd1ae4a59218e
Z-HongYuan <1939218189@qq.com>
2025-06-10 20:33:27 +0800

    添加作弊管理器

---
+++ Source/TwoGame/Character/Player/TwoCheatManager.h
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CheatManager.h"
+#include "TwoCheatManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWOGAME_API UTwoCheatManager : public UCheatManager
+{
+	GENERATED_BODY()
+};
