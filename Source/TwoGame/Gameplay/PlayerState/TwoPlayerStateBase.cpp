9d5a655e4e6c7c3ce92fd6be009030d6ddc87be9
Z-HongYuan <1939218189@qq.com>
2025-06-10 20:03:20 +0800

    添加PlayerState

---
+++ Source/TwoGame/Gameplay/PlayerState/TwoPlayerStateBase.cpp
@@ -0,0 +1,15 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+
+#include "TwoPlayerStateBase.h"
+
+ATwoPlayerStateBase::ATwoPlayerStateBase()
+{
+	bReplicates = true; //设置复制
+	SetNetUpdateFrequency(200.f); //设置网络同步速度
+	SetMinNetUpdateFrequency(100.f); //设置网络同步速度
+
+	AbilitySystemComponent = CreateDefaultSubobject<UTwoAbilitySystemComponentBase>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true); //启动复制
+	AttributeSet = CreateDefaultSubobject<UTwoAttributeSetBase>(TEXT("AttributeSet"));
+}
---
+++ Source/TwoGame/Gameplay/PlayerState/TwoPlayerStateBase.h
@@ -0,0 +1,37 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/PlayerState.h"
+#include "TwoGame/AbilitySystem/TwoAbilitySystemComponentBase.h"
+#include "TwoGame/AbilitySystem/Attribute/TwoAttributeSetBase.h"
+#include "TwoPlayerStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWOGAME_API ATwoPlayerStateBase : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	ATwoPlayerStateBase();
+	
+	/*
+	 * ASC和Attribute
+	 */
+	virtual UTwoAbilitySystemComponentBase* GetAbilitySystemComponent() const override { return AbilitySystemComponent; }
+	UTwoAttributeSetBase* GetAttributeSet() const { return AttributeSet; }
+
+protected:
+	/*
+	 * ASC和Attribute
+	 */
+	UPROPERTY()
+	TObjectPtr<UTwoAbilitySystemComponentBase> AbilitySystemComponent;
+	UPROPERTY()
+	TObjectPtr<UTwoAttributeSetBase> AttributeSet;
+};
