2f24f6dd5485eafa35f9e2a556c37ec3ed20a0c9
Z-HongYuan <1939218189@qq.com>
2025-06-10 19:11:38 +0800

    添加消散Actor

---
+++ Source/TwoGame/Actors/TwoFadingActor.cpp
@@ -0,0 +1,73 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+
+#include "TwoFadingActor.h"
+#include "TwoGame/Character/TwoCharacterBase.h"
+
+
+ATwoFadingActor::ATwoFadingActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	PoseableMesh = CreateDefaultSubobject<UPoseableMeshComponent>(TEXT("PoseableMesh"));
+
+	FadeMaterialStartValue = -1.0f;
+	FadeMaterialEndValue = 1.0f;
+	FadeStartDelayTime = FadeEndDelayTime = 1.0f;
+	bIsBeginFade = false;
+	FadeMaterialName = "FadingAlphaControl";
+}
+
+void ATwoFadingActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ATwoCharacterBase* CharacterBase = Cast<ATwoCharacterBase>(GetOwner()))
+	{
+		PoseableMesh->SetSkinnedAssetAndUpdate(CharacterBase->GetCoverMesh()->GetSkeletalMeshAsset());
+		PoseableMesh->CopyPoseFromSkeletalComponent(CharacterBase->GetCoverMesh());
+
+		//淡化前的延迟
+		GetWorldTimerManager().SetTimer(FadeStartDelayTimerHandle, this, &ThisClass::FadeStartDelay, 1, false, FadeStartDelayTime);
+	}
+	else
+	{
+		//log
+		UE_LOG(LogTemp, Warning, TEXT("AGenshinFadingActor::BeginPlay() Owner is not GenshinCharacter"));
+		this->Destroy();
+	}
+}
+
+void ATwoFadingActor::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (FadeStartDelayTimerHandle.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(FadeStartDelayTimerHandle);
+		FadeStartDelayTimerHandle.Invalidate();
+	}
+	if (FadeEndDelayTimerHandle.IsValid())
+	{
+		GetWorldTimerManager().ClearTimer(FadeEndDelayTimerHandle);
+		FadeEndDelayTimerHandle.Invalidate();
+	}
+
+	Super::EndPlay(EndPlayReason);
+}
+
+void ATwoFadingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bIsBeginFade)
+	{
+		FadeMaterialStartValue = FMath::FInterpTo(FadeMaterialStartValue, FadeMaterialEndValue, DeltaTime, FadeSpeed);
+		PoseableMesh->SetScalarParameterValueOnMaterials(FadeMaterialName, FadeMaterialStartValue);
+	}
+
+	//淡化结束的定时器回调,无论如何,只会执行一次
+	if (FMath::IsNearlyEqual(FadeMaterialStartValue, FadeMaterialEndValue, 0.001f) and bIsBeginFade)
+	{
+		bIsBeginFade = false;
+		GetWorldTimerManager().SetTimer(FadeEndDelayTimerHandle, this, &ThisClass::FadeEndDelay, 1, false, FadeEndDelayTime);
+	}
+}
---
+++ Source/TwoGame/Actors/TwoFadingActor.h
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2025 鸿源z. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/PoseableMeshComponent.h"
+#include "GameFramework/Actor.h"
+#include "TwoFadingActor.generated.h"
+
+UCLASS()
+class TWOGAME_API ATwoFadingActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ATwoFadingActor();
+
+	//材质淡出的开始值,默认为-1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade")
+	float FadeMaterialStartValue;
+
+	//材质淡出的结束值,默认为1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade")
+	float FadeMaterialEndValue;
+
+	//需要修改的材质参数值名称,默认为为"BodyAlphaControl"
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade")
+	FName FadeMaterialName;
+
+	//淡出开始前等待的延迟,默认为1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade")
+	float FadeStartDelayTime;
+
+	//淡出完成后结束延迟,最后销毁,默认为1
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade")
+	float FadeEndDelayTime;
+
+	//淡化速度
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fade")
+	float FadeSpeed;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PoseableMesh")
+	TObjectPtr<UPoseableMeshComponent> PoseableMesh;
+
+	//淡出控制bool
+	bool bIsBeginFade;
+
+	//定时器句柄
+	FTimerHandle FadeStartDelayTimerHandle;
+	FTimerHandle FadeEndDelayTimerHandle;
+
+	//延迟开始定时器回调,控制淡出bool
+	UFUNCTION()
+	void FadeStartDelay() { bIsBeginFade = true; }
+
+	//延迟结束定时器回调,控制销毁自身
+	UFUNCTION()
+	void FadeEndDelay() { this->Destroy(); }
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
